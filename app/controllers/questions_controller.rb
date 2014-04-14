a15e2c38484982c0450be12a8626af1056189698
Aaron <wikea1@gmail.com>
2014-04-14 16:21:19 -0700

    ajax functionality added for users to add questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,26 @@
 class QuestionsController < ApplicationController
   def index
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      respond_to do |format|
+        format.html { redirect_to user_path(current_user) }
+        format.js
+      end
+    else
+      render 'users/show'
+    end
+  end
+
+private
+  def question_params
+    params.require(:question).permit(:title, :description)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @posted_questions = Question.all
+
   end
 
 private
